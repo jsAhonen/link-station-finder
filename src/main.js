e9c7493ca2d52adbc1c2b8d311b2addf7078faf5
Jukka Ahonen <jukka.samuel@gmail.com>
2021-04-19 11:58:39 +0300

    Separate functions and data into separate files

--- src/main.js
+++ src/main.js
@@ -1,53 +1,5 @@
-const { distance } = require("mathjs");
-
-const linkStations = [
-  {
-    position: [0, 0],
-    reach: 10,
-  },
-  {
-    position: [20, 20],
-    reach: 5,
-  },
-  {
-    position: [10, 0],
-    reach: 12,
-  },
-];
-
-function getOptimalLinkStation(linkStations, position) {
-  const ls = linkStations
-    .filter((ls) => distance(ls.position, position) < ls.reach)
-    .reduce((a, b) => {
-      if (a && calculatePower(a, position) > calculatePower(b, position)) {
-        return a;
-      }
-      return b;
-    }, null);
-  return ls;
-}
-
-function calculatePower(linkStation, devicePosition) {
-  const dist = distance(linkStation.position, devicePosition);
-  if (dist > linkStation.reach) {
-    return 0;
-  }
-  const power = Math.pow(linkStation.reach - dist, 2);
-  return Math.max(power, 0);
-}
-
-function output(linkStations, position) {
-  const ls = getOptimalLinkStation(linkStations, position);
-  if (!ls) {
-    return `No link station within reach for point (${position.join(", ")}).`;
-  }
-  return `Best link station for point (${position.join(
-    ", "
-  )}) is at point (${ls.position.join(", ")}) with power ${calculatePower(
-    ls,
-    position
-  )}`;
-}
+const { output } = require("./utils");
+const { linkStations } = require("./data");
 
 linkStations.forEach((ls) => {
   console.log(`(${ls.position.join(", ")}), reach: ${ls.reach}\n`);
